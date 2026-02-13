e89365da270a3cd2e2882a34404f7d129d325aa6
vivoza9855-collab <vivoza9855@gmail.com>
2026-02-13 10:54:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -7,5 +8,7 @@ def home():
     return "Repair System Online 🚀"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
 
